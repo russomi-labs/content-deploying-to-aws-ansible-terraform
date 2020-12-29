e31e7ecffc74bd45a97047d521e11c1061e64ba1
Michael Russo <russo.mail@gmail.com>
2020-12-29 14:37:46 -0500

    Removes --profile paramter source credentials from environment

--- labs/instances.tf
+++ labs/instances.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "jenkins-master" {
   # Jenkins Master Provisioner:
   provisioner "local-exec" {
     command = <<EOF
-aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id}
+aws ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id}
 ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-master-sample.yml
 EOF
   }
@@ -63,7 +63,7 @@ resource "aws_instance" "jenkins-worker-oregon" {
   # inserted inside the resource block for Jenkins EC2 worker in Terraform
   provisioner "local-exec" {
     command = <<EOF
-aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-worker} --instance-ids ${self.id}
+aws ec2 wait instance-status-ok --region ${var.region-worker} --instance-ids ${self.id}
 ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-worker-sample.yml
 EOF
   }
